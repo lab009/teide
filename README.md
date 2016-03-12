409dc6ae698f9d226dd390c7dacae67097d83e2e
Oleg Orlov <mail@orlovoleg.com>
2016-03-13 01:47:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# tango
+
+> Toolkit for building redux/react applications
+
+### Package
+
+* react
+* react-redux
+* immutable
+* redux-immutablejs
+* react-immutable-proptypes
+* redux-thunk
+* redux-actions
+* reduce-reducers
+* lodash.foreach
+* lodash.mapvalues
+* lodash.map
+* lodash.reduce
+* lodash.filter
+* lodash.values
---
+++ index.js
@@ -0,0 +1,39 @@
+import { Component as ReactComponent, PropTypes as RPropTypes } from 'react';
+import { Provider } from 'react-redux';
+import IPropTypes from 'react-immutable-proptypes';
+import bindClass from './lib/bindClass';
+import connect from './lib/connect';
+import createStore from './createStore';
+import createActions from './lib/createActions';
+import createReducer from './lib/createReducer';
+import createReducerActions from './lib/createReducerActions';
+import combineReducers from './lib/combineReducers';
+
+class TangoComponent extends ReactComponent {
+  static defaultState = {};
+  constructor() {
+    super(...arguments);
+    this.state = {
+      ...this.constructor.defaultState,
+      ...this.state
+    };
+    bindClass(this);
+  }
+}
+
+const PropTypes = {
+  ...RPropTypes,
+  ...IPropTypes
+};
+
+export {
+  TangoComponent as Component,
+  connect,
+  PropTypes,
+  Provider,
+  combineReducers,
+  createReducer,
+  createReducerActions,
+  createActions,
+  createStore
+};
