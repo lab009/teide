248d9d0ae03241d7d4ecae64eff321cd123b8dd7
Oleg Orlov <mail@orlovoleg.com>
2016-11-02 13:44:58 +0300

    build

--- es/lib/bindClass.js
+++ es/lib/bindClass.js
@@ -1,8 +1,1 @@
-import _Object$getOwnPropertyNames from 'babel-runtime/core-js/object/get-own-property-names';
-export default (function (context) {
-  return _Object$getOwnPropertyNames(context.constructor.prototype).filter(function (method) {
-    return typeof context[method] === 'function' && method !== 'constructor';
-  }).forEach(function (method) {
-    context[method] = context[method].bind(context);
-  });
-});
+export default(function(context){return Object.getOwnPropertyNames(context.constructor.prototype).filter(function(method){return typeof context[method]==='function'&&method!=='constructor';}).forEach(function(method){context[method]=context[method].bind(context);});});
--- es/lib/combineReducers.js
+++ es/lib/combineReducers.js
@@ -1,15 +1,1 @@
-import { combineReducers } from '@eagle/redux-immutablejs';
-import map from 'lodash.map';
-import reduceReducers from 'reduce-reducers';
-
-var combine = function combine() {
-  for (var _len = arguments.length, reducers = Array(_len), _key = 0; _key < _len; _key++) {
-    reducers[_key] = arguments[_key];
-  }
-
-  return reduceReducers.apply(undefined, map(reducers, function (v) {
-    return typeof v === 'function' ? v : combineReducers(v);
-  }));
-};
-
-export default combine;
+import{combineReducers}from'@eagle/redux-immutablejs';import map from'lodash.map';import reduceReducers from'reduce-reducers';var combine=function combine(){for(var _len=arguments.length,reducers=Array(_len),_key=0;_key<_len;_key++){reducers[_key]=arguments[_key];}return reduceReducers.apply(undefined,map(reducers,function(v){return typeof v==='function'?v:combineReducers(v);}));};export default combine;
