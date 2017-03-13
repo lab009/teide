e8e4e5b721023f5d25ef8e8375120a877bc9939f
Oleg Orlov <mail@orlovoleg.com>
2017-03-13 15:49:38 +0300

    Use custom combineReducers for Immutable (#1)

--- packages/teide/src/lib/combineReducers.js
+++ packages/teide/src/lib/combineReducers.js
@@ -1,6 +1,29 @@
-import { combineReducers } from 'redux-immutable'
+import Immutable from 'immutable'
 import map from 'lodash.map'
 import reduceReducers from 'reduce-reducers'
+
+const validateNextState = (nextState, reducerName, action) => {
+  if (nextState === undefined) {
+    throw new Error(`Reducer "${reducerName}" returned undefined when handling "${action.type}" action. To ignore an action, you must explicitly return the previous state.`)
+  }
+}
+
+const combineReducers = (reducers, getDefaultState = Immutable.Map) => {
+  const reducerKeys = Object.keys(reducers)
+
+  return (inputState = getDefaultState(), action) =>
+    inputState.withMutations((temporaryState) => {
+      reducerKeys.forEach((reducerName) => {
+        const reducer = reducers[reducerName]
+        const currentDomainState = temporaryState.get(reducerName)
+        const nextDomainState = reducer(currentDomainState, action)
+
+        validateNextState(nextDomainState, reducerName, action)
+
+        temporaryState.set(reducerName, nextDomainState)
+      })
+    })
+}
 
 const combine = (...reducers) =>
   reduceReducers(...map(reducers, v => (typeof v === 'function' ? v : combineReducers(v))))
