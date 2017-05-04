44fe0a957152137ba344a1936742f663df005d01
Oleg Orlov <mail@orlovoleg.com>
2017-05-04 16:53:07 +0300

    Fix mergeProps order, own props should always be top dog (#14)

--- packages/teide/src/lib/connect.js
+++ packages/teide/src/lib/connect.js
@@ -3,13 +3,15 @@ import resolve from './resolveStoreProps'
 
 const mapStateToProps = storeProps => (storeState, ownProps) => resolve(storeProps, storeState, ownProps)
 
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({ ...stateProps, ...dispatchProps, ...ownProps })
+
 const defaults = {
   pure: true,
   withRef: false,
 }
 
 export default (storeProps, options) => {
-  const connector = connect(storeProps ? mapStateToProps(storeProps) : null, null, null, { ...defaults, ...options })
+  const connector = connect(storeProps ? mapStateToProps(storeProps) : null, null, mergeProps, { ...defaults, ...options })
   return (Component) => {
     Component.storeProps = storeProps
     return connector(Component)
